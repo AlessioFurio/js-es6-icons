a4c8aa207f419bd1145f7cd312a0e13a8a53044d
Furio Alessio <alefurio@hotmail.it>
2020-11-05 14:57:28 +0100

    visualizzazione in pagina di tutte le icone con il proprio nome.

---
+++ main.js
@@ -0,0 +1,131 @@
+
+$(document).ready(function(){
+
+    // definire un array di oggetti;
+    // ogni oggetto rappresenta un'icona, che è caratterizzata da: nome, prefisso, tipo e famiglia.
+    const arrayIcons = [
+        {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    }
+    ];
+
+    // Utilizzando la funzione forEach e il template literal, visualizzare in pagina tutte le icone con il proprio nome.
+
+    arrayIcons.forEach((item, i) => { // utilizzo il forEach per scorrere tutti gli oggetti nell'arrayIcons
+
+         // stampo in console tutti gli oggetti con relativo nome
+        console.log(`
+            ${item['name']}
+        `);
+
+        // stampo in pagina tutti gli oggetti con relativo nome
+        $('.containerIconsNomi').append(`
+            <div>${item['name']}</div>
+            `)
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+});
